f40648247753d5177e206d02a1446eefa7a52b4b
Wbaidur Rahman <wbaidurrahman2000@gmail.com>
2022-03-09 11:56:53 +0600

    First step on NodeJs

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const PORT = (process.env.PORT || 3005)
+app.use(express.urlencoded({
+  extended: "false"
+}))
+app.use(express.json())
+app.use('/dishes',require('./routes/dishRouter'))
+app.use('/promotions',require('./routes/promoRouter'))
+app.use('/leader',require('./routes/leaderRouter'))
+app.all('/', (req, res) => {
+  res.send('Index Page')
+})
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`)
+})
---
+++ routes/leaderRouter.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const leaderRouter = express.Router();
+const bodyParser = require('body-parser');
+ 
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.all((req, res) => {
+		res.send(`${req.method} request on Leader router`)
+	})
+
+leaderRouter.route('/:leaderId')
+.get((req,res) => {
+	res.send('Get request on Leader Id = ${req.params.leaderId}')
+})
+
+.post((req,res) => {
+	res.send('Post request on Leader Id = ${req.params.leaderId}')
+})
+
+.put((req,res) => {
+	res.send('Put request on Leader Id = ${req.params.leaderId}')
+})
+
+.delete((req,res) => {
+	res.send('Delete request on Leader Id = ${req.params.leaderId}')
+})
+
+.all((req, res) => {
+		res.send('Invalid Request')
+	})
+
+module.exports = leaderRouter;
