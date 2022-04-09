3d648b21fb4371292b5549b36c52c04a4915ff54
Wbaidur-Rahman <94358161+Wbaidur-Rahman@users.noreply.github.com>
2022-04-10 00:35:13 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const express = require('express')
+const express = require("express")
+const mongoose = require('mongoose')
+
+const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
+
+const url = "mongodb://localhost:27017/nodeApp";
+
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(url, () => {
+
+    console.log('Connected correctly to server');
+
+});
+
 const app = express()
-const PORT = (process.env.PORT || 3000)
-app.use(express.urlencoded({
-  extended: "false"
-}))
-app.use(express.json())
-app.use('/dishes',require('./routes/dishRouter'))
-app.use('/promotions',require('./routes/promoRouter'))
-app.use('/leaders',require('./routes/leaderRouter'))
-app.all('/', (req, res) => {
-  res.send('This is an express server!')
-})
-app.listen(PORT, () => {
-  console.log(`Server running on PORT ${PORT}`)
-})
+
+app.use(express.json());
+
+app.use('/dishes', dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders', leaderRouter);
+
+app.listen(3000, () =>{
+	console.log("Listening on port 3000...");
+});
+
